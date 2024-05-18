76bedb0d2f327861907817facbe4dfa5099d4123
SerigneFallouNiang <fallouniang776@gmail.com>
2024-05-18 20:48:14 +0000

    projet github

---
+++ 20240518_sitedetestf_db7e28e4bcc2d64a1517_20240518204058_archive/wp-content/plugins/hostinger/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'hostinger/hostinger',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'ff32119ecdc581fd25c2cf1b9881120a8042f7e5',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'hostinger/hostinger' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'ff32119ecdc581fd25c2cf1b9881120a8042f7e5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
